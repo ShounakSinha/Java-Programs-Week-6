f1c52b348602edefbe58f3dbeb1b6bb10c175132
Shounak Sinha <126563210+ShounakSinha@users.noreply.github.com>
2024-12-08 01:03:48 +0530

    Update dynamicmethoddispatch.java

--- dynamicmethoddispatch.java
+++ dynamicmethoddispatch.java
@@ -1,1 +1,21 @@
+class Animal {
+    void makeSound() {
+        System.out.println("Generic animal sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("Cat meows");
+    }
+}
+
 
