422a95da9de7a7a66d17550cb996fc0dd78193d6
Shounak Sinha <126563210+ShounakSinha@users.noreply.github.com>
2024-12-10 00:32:01 +0530

    Update MainCalculator.java

--- MainCalculator.java
+++ MainCalculator.java
@@ -1,1 +1,12 @@
+import mypackage.Calculator;
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        Calculator calc = new Calculator();
+        System.out.println("Addition: " + calc.add(5, 3));
+        System.out.println("Subtraction: " + calc.subtract(5, 3));
+    }
+}
 
