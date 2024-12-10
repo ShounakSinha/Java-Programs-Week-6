322d5e748b755acaa84995067e19ce1e78c9335a
Shounak Sinha <126563210+ShounakSinha@users.noreply.github.com>
2024-12-11 01:17:17 +0530

    Update ArrayIndexOutofBound.java

--- ArrayIndexOutofBound.java
+++ ArrayIndexOutofBound.java
@@ -1,1 +1,10 @@
-
+public class Main {
+    public static void main(String[] args) {
+        try {
+            int[] array = {1, 2, 3};
+            System.out.println("Element at index 5: " + array[5]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Error: Index is out of bounds");
+        }
+    }
+}
