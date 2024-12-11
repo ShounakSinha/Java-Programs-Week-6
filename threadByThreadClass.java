41ad93969d77fc6eae8d1ebb50f716597d547fd1
Shounak Sinha <126563210+ShounakSinha@users.noreply.github.com>
2024-12-12 00:44:01 +0530

    Update threadByThreadClass.java

--- threadByThreadClass.java
+++ threadByThreadClass.java
@@ -1,1 +1,14 @@
+class MyThread extends Thread {
+    public void run() {
+        System.out.println("This thread is running!");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        MyThread thread = new MyThread(); // Create a new thread
+        thread.start(); // Start the thread
+        System.out.println("Main program is running!");
+    }
+}
 
