241071eec4e98ee9e68615ebdd4bbfbc5bbf3317
Shounak Sinha <126563210+ShounakSinha@users.noreply.github.com>
2024-12-12 00:51:57 +0530

    Update chatApplicationUsingJava.java

--- chatApplicationUsingJava.java
+++ chatApplicationUsingJava.java
@@ -1,1 +1,24 @@
+class MessageReceiver extends Thread {
+    public void run() {
+        while (true) {
+            System.out.println("New message received!");
+            try { Thread.sleep(2000); } catch (InterruptedException e) { }
+        }
+    }
+}
+
+class MessageSender extends Thread {
+    public void run() {
+        System.out.println("You can type and send messages.");
+    }
+}
+
+public class ChatApp {
+    public static void main(String[] args) {
+        MessageReceiver receiver = new MessageReceiver();
+        MessageSender sender = new MessageSender();
+        receiver.start();
+        sender.start();
+    }
+}
 
