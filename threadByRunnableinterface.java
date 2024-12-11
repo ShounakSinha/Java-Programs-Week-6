740ebd7945bb262af82ddff88cbddfc4f50b6a1b
Shounak Sinha <126563210+ShounakSinha@users.noreply.github.com>
2024-12-12 00:47:32 +0530

    Update threadByRunnableinterface.java

--- threadByRunnableinterface.java
+++ threadByRunnableinterface.java
@@ -1,1 +1,15 @@
+class MyRunnable implements Runnable {
+    public void run() {
+        System.out.println("This thread is running!");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        MyRunnable myRunnable = new MyRunnable();
+        Thread thread = new Thread(myRunnable); // Create a Thread object
+        thread.start(); // Start the thread
+        System.out.println("Main program is running!");
+    }
+}
 
