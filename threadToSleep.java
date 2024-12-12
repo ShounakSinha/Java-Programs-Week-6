b817c7eb3b1f27f4c1d9107c1f9b9e3d5136ad12
Shounak Sinha <126563210+ShounakSinha@users.noreply.github.com>
2024-12-13 00:56:06 +0530

    Update threadToSleep.java

--- threadToSleep.java
+++ threadToSleep.java
@@ -1,1 +1,9 @@
-
+class MyThread extends Thread {
+    public void run() {
+        try {
+            for (int i = 1; i <= 5; i++) {
+                System.out.println("Running: " + i);
+                Thread.sleep(1000); // Pause for 1 second
+            }
+        } catch (InterruptedException e) {
+            System.out
