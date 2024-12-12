cf41e11512e040e93c50e990e4858ca511dc157a
Shounak Sinha <126563210+ShounakSinha@users.noreply.github.com>
2024-12-13 01:03:13 +0530

    Update threadToFinish.java

--- threadToFinish.java
+++ threadToFinish.java
@@ -1,1 +1,28 @@
+class MyThread extends Thread {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Running: " + i);
+            try {
+                Thread.sleep(1000); // Pause for 1 second
+            } catch (InterruptedException e) {
+                System.out.println("Thread was interrupted!");
+            }
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        MyThread thread = new MyThread();
+        thread.start();
+
+        try {
+            thread.join(); // Wait for the thread to complete
+        } catch (InterruptedException e) {
+            System.out.println("Main thread interrupted!");
+        }
+
+        System.out.println("Thread has finished, now resuming main thread.");
+    }
+}
 
